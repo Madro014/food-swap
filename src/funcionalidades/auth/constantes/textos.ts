5d7a97581e676ea800a3a2a0779e0a46f72e4f58
Madro014 <spinnercrack1@gmail.com>
2026-03-11 18:54:28 -0500

    feat: actualizar componentes auth y estilos

---
+++ src/funcionalidades/auth/constantes/textos.ts
@@ -0,0 +1,24 @@
+export const TEXTOS_AUTH = {
+    login: {
+        subtituloLogo: "Encuentra el sabor que buscas",
+        labelEmail: "Correo Electrónico",
+        placeholderEmail: "tu@correo.com",
+        labelPassword: "Contraseña",
+        placeholderPassword: "********",
+        botonSubmit: "Entrar",
+        textoEnlaceNormal: "¿No tienes cuenta? ",
+        textoEnlaceBold: "Regístrate",
+    },
+    registro: {
+        subtituloLogo: "Crea una cuenta para empezar",
+        labelNombre: "Nombre Completo",
+        placeholderNombre: "Juan Pérez",
+        labelEmail: "Correo Electrónico",
+        placeholderEmail: "tu@correo.com",
+        labelPassword: "Contraseña",
+        placeholderPassword: "********",
+        botonSubmit: "Registrarse",
+        textoEnlaceNormal: "¿Ya tienes cuenta? ",
+        textoEnlaceBold: "Inicia Sesión",
+    }
+};
